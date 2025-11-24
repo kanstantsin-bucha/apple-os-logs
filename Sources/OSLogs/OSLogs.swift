3997681b6d74ab211ca1884ea8efe3e4cca5d927
Kanstantsin Bucha <kanstantsin.bucha@withintent.com>
2025-11-24 13:00:27 +0300

    explicit mark the platform availability and add the hook/destinations to the Logs.

--- Sources/OSLogs/OSLogs.swift
+++ Sources/OSLogs/OSLogs.swift
@@ -1,8 +1,11 @@
 import OSLog
 import Foundation
 
+@available(macOS 12.0, iOS 15.0, *)
 public enum Logs {
+    public typealias LogDestination = (OSLogType, _ category: String, _ message: String) -> Void
     public static let main = AppLogger(category: "") // General
+    fileprivate static var logDestinations: [LogDestination] = []
 
     /// Gets the App logs from the system
     /// - Parameter interval: The time from now into the past (default is 1 hour)
@@ -21,6 +24,13 @@ public enum Logs {
             main.log("Failed to get logs data with error: \(error)")
             return Data()
         }
+    }
+    
+    /// The way to add a hook to all app logs going into the system
+    /// - Parameter logDestination: the closure that will be called for every log logged in the App
+    @MainActor
+    public static func add(logDestination: @escaping LogDestination) {
+        logDestinations.append(logDestination)
     }
 
     private static func getLogEntries(interval: TimeInterval, isFullLog: Bool = false) throws -> [OSLogEntryLog] {
@@ -42,12 +52,15 @@ public enum Logs {
     }
 }
 
+@available(macOS 12.0, iOS 15.0, *)
 public struct AppLogger {
     static let subsystem = Bundle.main.bundleIdentifier ?? "OSLogger"
     private let logger: Logger
+    private let category: String
 
     public init(category: String) {
-        self.logger = Logger(subsystem: Self.subsystem, category: category)
+        logger = Logger(subsystem: Self.subsystem, category: category)
+        self.category = category
     }
 
     public func log(_ message: String, file: String = #fileID, function: String = #function) {
@@ -65,15 +78,18 @@ public struct AppLogger {
         // 2023-06-20 10:58:58 am +0000: notice: subsystem-category:   [<private> configureTimeControlStatusObserver()] <private> -> 6240034
         // OS hides too much right now, so we override our logs to be public
         logger.log(level: level, "\(message, privacy: .public)")
+        Logs.logDestinations.forEach { $0(level, category, message) }
     }
 }
 
+@available(macOS 12.0, iOS 15.0, *)
 fileprivate extension OSLogEntryLog {
     var logString: String {
         "\(date): \(level.logString): \(subsystem)-\(category):   \(composedMessage) -> \(threadIdentifier)\r\n"
     }
 }
 
+@available(macOS 12.0, iOS 15.0, *)
 fileprivate extension OSLogEntryLog.Level {
     var logString: String {
         switch self {
